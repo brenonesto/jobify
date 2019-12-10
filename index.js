1f4c9a846c89360a2b818eb5bc9992c8238f23aa
Breno Nesto <bnesto1@gmail.com>
2019-12-09 23:35:47 -0300

    Fixing bugs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const port = process.env.PORT || 3000
 
